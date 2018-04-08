0677b318077010bd64a4b7e91e4a42c533636106
star <3434234550@qq.com>
2018-04-08 18:40:03 +0800

    工作台动态读取

--- public/static/js/echart_data.js
+++ public/static/js/echart_data.js
@@ -1,12 +1,51 @@
 var dom = document.getElementById("visitor1");
 var myChart = echarts.init(dom);
 var app = {};
+
+//客户状态一
 var customer_status_1=document.getElementsByClassName("customer_status_1");
 var customer_status_1_length = customer_status_1.length;
 var customer_status_1_key = [];
 var customer_status_1_value = [];
 for(var i=0;i<customer_status_1_length;i++){
-    customer_status_1_key[i] =customer_status_1[i].value;
+    customer_status_1_key[i] =customer_status_1[i].name;
+
+    customer_status_1_value[i] =  {value:customer_status_1[i].value, name:customer_status_1[i].name};
+}
+
+//客户状态二
+var customer_status_2=document.getElementsByClassName("customer_status_2");
+var customer_status_2_length = customer_status_2.length;
+var customer_status_2_key = [];
+var customer_status_2_value = [];
+for(var i=0;i<customer_status_2_length;i++){
+    customer_status_2_key[i] =customer_status_2[i].name;
+
+    customer_status_2_value[i] =  {value:customer_status_2[i].value, name:customer_status_2[i].name};
+}
+
+
+//合同状态
+var contract_status=document.getElementsByClassName("contract_status");
+var contract_status_length = contract_status.length;
+var contract_status_key = [];
+var contract_status_value = [];
+for(var i=0;i<contract_status_length;i++){
+    contract_status_key[i] =contract_status[i].name;
+
+    contract_status_value[i] =  {value:contract_status[i].value, name:contract_status[i].name};
+}
+
+
+//项目统计
+var product_demand=document.getElementsByClassName("product_demand_1");
+var product_demand_length = product_demand.length;
+var product_demand_key = [];
+var product_demand_value = [];
+for(var i=0;i<product_demand_length;i++){
+    product_demand_key[i] =product_demand[i].name;
+
+    product_demand_value[i] =  {value:product_demand[i].value, name:product_demand[i].name};
 }
 
 
@@ -24,7 +63,8 @@ option = {
     legend: {
         orient: 'vertical',
         left: 'left',
-        data: ['潜在客户','需求客户','提案客户','签约客户']
+       // data: ['潜在客户','需求客户','提案客户','签约客户']
+        data: customer_status_1_key
     },
     series : [
         {
@@ -32,12 +72,13 @@ option = {
             type: 'pie',
             radius : '55%',
             center: ['50%', '60%'],
-            data:[
-                {value:document.getElementById('潜在客户').value, name:'潜在客户'},
-                {value:document.getElementById('需求客户').value, name:'需求客户'},
-                {value:document.getElementById('提案客户').value, name:'提案客户'},
-                {value:document.getElementById('签约客户').value, name:'签约客户'}
-            ],
+            // data:[
+            //     {value:document.getElementById('潜在客户').value, name:'潜在客户'},
+            //     {value:document.getElementById('需求客户').value, name:'需求客户'},
+            //     {value:document.getElementById('提案客户').value, name:'提案客户'},
+            //     {value:document.getElementById('签约客户').value, name:'签约客户'}
+            // ],
+            data:customer_status_1_value,
 
             itemStyle: {
                 emphasis: {
@@ -72,7 +113,8 @@ option = {
     legend: {
         orient: 'vertical',
         left: 'left',
-        data: ['战略级客户','品牌级客户','核心级客户','普通级客户']
+      //  data: ['战略级客户','品牌级客户','核心级客户','普通级客户']
+        data: customer_status_2_key
 
     },
     series : [
@@ -81,12 +123,13 @@ option = {
             type: 'pie',
             radius : '55%',
             center: ['50%', '60%'],
-            data:[
-                {value:document.getElementById('战略级客户').value, name:'战略级客户'},
-                {value:document.getElementById('品牌级客户').value, name:'品牌级客户'},
-                {value:document.getElementById('核心级客户').value, name:'核心级客户'},
-                {value:document.getElementById('普通级客户').value, name:'普通级客户'}
-            ],
+            // data:[
+            //     {value:document.getElementById('战略级客户').value, name:'战略级客户'},
+            //     {value:document.getElementById('品牌级客户').value, name:'品牌级客户'},
+            //     {value:document.getElementById('核心级客户').value, name:'核心级客户'},
+            //     {value:document.getElementById('普通级客户').value, name:'普通级客户'}
+            // ],
+            data:customer_status_2_value,
             itemStyle: {
                 emphasis: {
                     shadowBlur: 10,
@@ -121,7 +164,7 @@ option = {
     legend: {
         orient: 'vertical',
         left: 'left',
-        data: ['合约审核','合约签署','合约执行','合约完成','续约审核','续约签署','续约执行','续约完成']
+        data: contract_status_key
     },
     series : [
         {
@@ -129,16 +172,7 @@ option = {
             type: 'pie',
             radius : '55%',
             center: ['50%', '60%'],
-            data:[
-                {value:document.getElementById('合约审核').value, name:'合约审核'},
-                {value:document.getElementById('合约签署').value, name:'合约签署'},
-                {value:document.getElementById('合约执行').value, name:'合约执行'},
-                {value:document.getElementById('合约完成').value, name:'合约完成'},
-                {value:document.getElementById('续约审核').value, name:'续约审核'},
-                {value:document.getElementById('续约签署').value, name:'续约签署'},
-                {value:document.getElementById('续约执行').value, name:'续约执行'},
-                {value:document.getElementById('续约完成').value, name:'续约完成'}
-            ],
+            data:contract_status_value,
             itemStyle: {
                 emphasis: {
                     shadowBlur: 10,
@@ -173,7 +207,7 @@ option = {
     legend: {
         orient: 'vertical',
         left: 'left',
-        data: ['品牌信息壁垒体系','创意策划传播','平面视觉','视频视觉','媒介资源采购','技术开发','整合营销顾问','效果营销优化','新媒体运营']
+        data: product_demand_key
     },
     series : [
         {
@@ -181,17 +215,7 @@ option = {
             type: 'pie',
             radius : '55%',
             center: ['50%', '60%'],
-            data:[
-                {value:document.getElementById('品牌信息壁垒体系').value, name:'品牌信息壁垒体系'},
-                {value:document.getElementById('创意策划传播').value, name:'创意策划传播'},
-                {value:document.getElementById('平面视觉').value, name:'平面视觉'},
-                {value:document.getElementById('视频视觉').value, name:'视频视觉'},
-                {value:document.getElementById('媒介资源采购').value, name:'媒介资源采购'},
-                {value:document.getElementById('技术开发').value, name:'技术开发'},
-                {value:document.getElementById('整合营销顾问').value, name:'整合营销顾问'},
-                {value:document.getElementById('效果营销优化').value, name:'效果营销优化'},
-                {value:document.getElementById('新媒体运营').value, name:'新媒体运营'}
-            ],
+            data:product_demand_value,
             itemStyle: {
                 emphasis: {
                     shadowBlur: 10,
