bdf247ac8a3a65cfddc6fbb3aee39debe09f640d
star <3434234550@qq.com>
2018-05-03 11:08:27 +0800

    岗位管理权限

--- application/index/logic/User.php
+++ application/index/logic/User.php
@@ -200,9 +200,10 @@ class User extends Model{
                 ->where("jckk_user.is_delete",0)
                 ->join("jckk_post","jckk_post.id=jckk_user.post_id")
                 ->join("jckk_department","jckk_department.id=jckk_user.department_id")
-                ->order("jckk_department.sort",'asc')
                 ->group("jckk_user.department_id")
                 ->group("jckk_user.uid")
+                ->order("jckk_department.sort",'asc')
+                ->order("jckk_post.sort",'asc')
                 ->paginate();
 
         return $users;
