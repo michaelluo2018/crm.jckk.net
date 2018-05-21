33f04d83c6ad33eeac67479dea6368de3fe70a86
star <3434234550@qq.com>
2018-05-21 16:56:06 +0800

    水印

--- application/index/controller/H5.php
+++ application/index/controller/H5.php
@@ -1,11 +1,11 @@
 <?php
 namespace  app\index\controller;
 
+use extend\ImageWatermark;
 use think\Config;
 use think\Controller;
-use Think\Image;
-
-
+use Think\Request;
+require ROOT_PATH .'/extend/ImageWaterMark.php';
 
 
 class H5 extends Controller {
@@ -17,12 +17,12 @@ class H5 extends Controller {
 
 
         public function save_result(){
-            $score = Request::instance()->post("score");
-            $name = Request::instance()->post("name");
+           // $score = Request::instance()->post("score");
+            //$name = Request::instance()->post("name");
             //分数总值除以6，然后判断区间，随机获取昵称，随机获取3行内容，生成结果图片
-            $average = array_sum($score)/6;
-            //$name = "小二郎";
-           // $average = 90;
+            //$average = array_sum($score)/6;
+            $name = "小二郎";
+            $average = 90;
             $theme_type = Config::get("theme_type");
             if($average>85){
                 //学神
@@ -40,16 +40,30 @@ class H5 extends Controller {
             $theme_result = model("theme","logic")->get_user_theme_result($user_theme);
             //dump($theme_result);die;
 
-            $orign_image = ROOT_PATH.'public'.DS.'static'.DS.'assets'.DS.'images'.DS.'1.jpg';
-            $result_image = ROOT_PATH.'public'.DS.'uploads'.DS.date('YmdHis').DS.$name.rand(1000,99999).'.jpg';
+            $orign_image = ROOT_PATH.'public'.DS.'static'.DS.'assets'.DS.'images'.DS.'background'.DS.'login-register=.jpg';
+            $result_file = ROOT_PATH.'public'.DS.'uploads'.DS.date('Ymd').DS;
+            if (!is_dir($result_file)) {
+                mkdir($result_file, 0755, true);
+            }
+            $result_image =rand(1000,99999).'.jpg';
             //生成图片
-            $image = Image::open($orign_image);
-            dump($image);die;
-            $image->text($name,'字库','大小','#fffff')->save($result_image);
-            $image->text($theme_result['theme'],'字库','大小','#fffff')->save($result_image);
-            $image->text($theme_result['describe1'],'字库','大小','#fffff')->save($result_image);
-            $image->text($theme_result['describe2'],'字库','大小','#fffff')->save($result_image);
-            $image->text($theme_result['describe3'],'字库','大小','#fffff')->save($result_image);
+            $image = new ImageWatermark($orign_image);
+            $image->setMarkPos(100,200);//设置setMarkPos,则markPosType无效。
+            $image->fontFile=ROOT_PATH.'public'.DS.'static'.DS.'font'.DS.'simkai.ttf';
+            $image->color='#FFFFFF';
+            $image->fontSize=24;
+            echo $theme_result['theme']['theme_name'];
+            $image->appendTextMark($theme_result['theme']['theme_name']);
+
+
+//            $image->appendTextMark($theme_result['describe1']);
+//            $image->appendTextMark($theme_result['describe2']);
+//            $image->appendTextMark($theme_result['describe3']);
+
+            $image->write($result_file.$result_image);
+
+
+
 
 
         }
